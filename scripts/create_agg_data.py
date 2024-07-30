cea33ced42b144e062f72c49f6d2e20ef32c19d5
pingilirishitha <rishithareddy5789@gmail.com>
2024-07-30 12:42:21 -0500

    Update create_agg_data.py

--- scripts/create_agg_data.py
+++ scripts/create_agg_data.py
@@ -1,4 +1,3 @@
-# create a aggregated view
 
 from django.db import connection
 raw_query = "create or replace view weather_yearlystats as select  max(id) as id,date_part('year', date) as year,station_id,avg(max_temperature) as avg_max_temperature,avg(min_temperature) as avg_min_temperature,sum(precipitation) as total_precipitation from public.weather_weatherrecord  group by year,station_id"
